0ccb5ef9bf01862c83000b8cdc914cc4dcae7fbc
Noviv <matthewwebb65@yahoo.com>
2015-11-30 15:07:25 -0600

    Added automatic WinPCAP install script

---
+++ checkinstall.bat
@@ -0,0 +1,27 @@
+@echo off
+cls
+set "a=1"
+if "%PCAP_ROOT_DIR%"==""  (
+	set "a=2"
+	echo PCAP_ROOT_DIR is not defined! WinPCAP needs to be installed.
+) else (
+	if not exist "%PCAP_ROOT_DIR%\\Include\\pcap\\pcap.h" (
+		set "a=2"
+		echo WinPCAP did not install properly, reinstall.
+	)
+)
+
+if "%a%"=="1" (
+	echo WinPCAP installed properly.
+) else (
+	echo WinPCAP needs to be [re]installed.
+	echo Would you like to install WinPCAP version 4.1.3 automatically? [y/n]
+	set /p choice="?:"
+	if "%choice%"=="y" (
+		echo install
+		bitsadmin /transfer WinPCAPInstall /download /priority normal https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe %TEMP%\\winpcap.exe
+		%TEMP%/winpcaptempinstall.exe
+		checkinstall.bat
+		del %TEMP%\\winpcap.exe
+	)
+)
